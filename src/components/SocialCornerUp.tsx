d3601696a4afa041b1482607f9f147ae7a8af3b3
Jean-Baptiste Lasselle <jean.baptiste.lasselle@gmail.com>
2023-11-07 02:56:33 +0100

    a bit more share socials drop up and drop down base

--- src/components/SocialCornerUp.tsx
+++ src/components/SocialCornerUp.tsx
@@ -1,13 +1,35 @@
 // import React from "preact/compat";
 import { TwitchIcon, YoutubeIcon, FacebookIcon,Share2Icon } from 'lucide-preact'
+import {
+  EmailShareButton,
+  FacebookShareButton,
+  HatenaShareButton,
+  InstapaperShareButton,
+  LineShareButton,
+  LinkedinShareButton,
+  LivejournalShareButton,
+  MailruShareButton,
+  OKShareButton,
+  PinterestShareButton,
+  PocketShareButton,
+  RedditShareButton,
+  TelegramShareButton,
+  TumblrShareButton,
+  TwitterShareButton,
+  ViberShareButton,
+  VKShareButton,
+  WhatsappShareButton,
+  WorkplaceShareButton
+} from "react-share";
 
 import { useState } from 'preact/compat'; // const [isOpen, setIsOpen] = useState<boolean>(true); // onClick={() => props.setIsOpen(true)}
 // import styles from '~/components/SocialCornerUp.module.css';
 // import SocialCornerUpDrawer from "~/components/SocialCornerUpDrawer";
 export interface SocialCornerUpProps {
+  urlOfLinkToShare: string
 }
 
-export default function SocialCornerUp() {
+export default function SocialCornerUp( { urlOfLinkToShare = `https://www.twitch.tv/Justin_Curieux`}: SocialCornerUpProps) {
   const [isOpen, setIsOpen] = useState<boolean>(true); // onClick={() => props.setIsOpen(true)}
   setIsOpen(false)
   const onClickHandler = () => {
@@ -19,23 +41,23 @@ export default function SocialCornerUp() {
       {// <!-- https://github.com/tholman/github-corners -->
       }
 
-      <div class="relative ml-3 px-2 m-3 mr-6">
+      <div class="relative ml-3 px-2 m-3 mr-6 separateur scroll-ml-14 origin-[right_center] scale-1 transition-all duration-500">
         <div>
           <button 
 
-          onClick={onClickHandler} 
-          type="button"
-          class="hover:cursor-pointer p-1 relative flex rounded-full text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
-          id="user-menu-button"
-          aria-expanded="false" 
-          aria-haspopup="true">
+            onClick={onClickHandler} 
+            type="button"
+            class="hover:cursor-pointer p-1 relative flex rounded-full text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
+            id="user-menu-button"
+            aria-expanded="false" 
+            aria-haspopup="true">
             <span class="absolute -inset-1.5"></span>
             <span class="sr-only">Open share menu</span>
 
             <Share2Icon
               size={48}
               strokeWidth={4}
-              stroke={`rgb(245, 119, 10, .9)`}
+              stroke={`currentColor`}
               //viewBox={"-6 -6 36 36"}
               viewBox={"-2 -4 32 32"}
               alt="Share"
@@ -69,12 +91,41 @@ export default function SocialCornerUp() {
          *   ></div>
          */}
         <div 
-          class="absolute top-[-150px] right-0 z-10 mt-2 w-48 rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none" role="menu" aria-orientation="vertical" aria-labelledby="user-menu-button" tabIndex={-1}>
+          class="absolute bottom-[75px] xl:bottom-[70px] right-7 md:right-0 lg:right-0 xl:right-0 2xl:right-0 z-10 mt-2 rounded-md bg-orange-500 py-1 shadow-lg ring-1 ring-orange ring-opacity-5 focus:outline-none" role="menu" aria-orientation="vertical" aria-labelledby="user-menu-button" tabIndex={-1}>
           {// <!-- Active: "bg-gray-100", Not Active: "" -->
           }
-          <a href="#" class="block px-4 py-2 text-sm text-gray-700" role="menuitem" tabIndex={-1} id="user-menu-item-0">Your Profile</a>
-          <a href="#" class="block px-4 py-2 text-sm text-gray-700" role="menuitem" tabIndex={-1} id="user-menu-item-1">Settings</a>
-          <a href="#" class="block px-4 py-2 text-sm text-gray-700" role="menuitem" tabIndex={-1} id="user-menu-item-2">Sign out</a>
+
+          <a href="#" class="block" role="menuitem" tabIndex={-1} id="user-menu-item-0">
+            <TwitchIcon
+              size={48}
+              strokeWidth={2}
+              stroke={`currentColor`}
+              //viewBox={"-6 -6 36 36"}
+              viewBox={"-2 -4 32 32"}
+              alt="Share"
+              className="h-18 w-18 m-1 p-1 rounded-full"
+            />
+          </a>
+          <a href="#" class="block" role="menuitem" tabIndex={-1} id="user-menu-item-1">
+            <YoutubeIcon               size={48}
+              strokeWidth={2}
+              stroke={`currentColor`}
+              //viewBox={"-6 -6 36 36"}
+              viewBox={"-2 -4 32 32"}
+              alt="Share"
+              className="h-18 w-18 m-1 p-1 rounded-full"
+            />
+          </a>
+          <a href="#" class="block" role="menuitem" tabIndex={-1} id="user-menu-item-2">
+            <FacebookIcon               size={48}
+              strokeWidth={2}
+              stroke={`currentColor`}
+              //viewBox={"-6 -6 36 36"}
+              viewBox={"-2 -4 32 32"}
+              alt="Share"
+              className="h-18 w-18 m-1 p-1 rounded-full"
+            />
+          </a>
         </div>
       </div>
 
