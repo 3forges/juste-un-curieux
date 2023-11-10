d9659a15a94528928c576a6d11530f009ead2fd3
bobo <bobo@pesto.io>
2023-11-10 07:42:51 +0100

    social into arc

--- src/components/SocialCornerUp.tsx
+++ src/components/SocialCornerUp.tsx
@@ -1,22 +1,25 @@
-// import React from "preact/compat";
 import { TwitchIcon, YoutubeIcon, FacebookIcon, Share2Icon } from 'lucide-preact'
-import { useState, useRef } from 'preact/compat'; // const [isOpen, setIsOpen] = useState<boolean>(true); // onClick={() => props.setIsOpen(true)}
+import { useState, useRef } from 'preact/compat'
 import DiscordIcon from "./DiscordIcon"
-import './SocialCornerUp.module.css';
 import { useEffect } from 'preact/hooks'
 
 export interface SocialCornerUpProps {
   urlOfLinkToShare: string
   name: string
-  headerHeight: number
 }
-// scale-0 transition-all duration-500
-const effet = `transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-300`
 
-export default function SocialCornerUp( { urlOfLinkToShare = `https://www.twitch.tv/Justin_Curieux`, name, headerHeight = 40 }: SocialCornerUpProps) {
-  const [isOpen, setIsOpen] = useState<boolean>(false); // onClick={() => props.setIsOpen(true)}
+const effet: string = `
+  transition-all 
+  ease-in-out 
+  delay-150 
+  hover:-translate-y-1 
+  hover:scale-110 
+  duration-300
+`
+
+export default function SocialCornerUp( { urlOfLinkToShare = `https://www.twitch.tv/Justin_Curieux`, name }: SocialCornerUpProps) {
+  const [isOpen, setIsOpen] = useState<boolean>(false)
   const [isUp, setIsUp] = useState<boolean>(true)
-  //const currentColor = "#535353"
   const menuCompo = useRef<any>(null)
 
   const onClickHandler = () => {
@@ -24,44 +27,34 @@ export default function SocialCornerUp( { urlOfLinkToShare = `https://www.twitch
     setIsOpen(!isOpen);
   }
 
-  const onLeaveHandler = () => {
-    //setIsOpen(false);
+  function evalscroll() {
+    const { innerWidth: width, innerHeight: height } = window;
+    let scroll = document.body.scrollTop || document.documentElement.scrollTop
+    // 88 = <li><share></li> height
+    // 30 = fixed
+    const d = innerHeight - scroll - menuCompo.current.clientHeight - 88 - 30
+    if (d < 0) setIsUp(false)
+    else setIsUp(true)
   }
 
   useEffect(() => {
-    document.addEventListener("scroll", () => {
-      const { innerWidth: width, innerHeight: height } = window;
-      let scroll = document.body.scrollTop || document.documentElement.scrollTop
-      console.log(`menuCompo.height : ${menuCompo.current.clientHeight}`, menuCompo)
-      const d = innerHeight - scroll - headerHeight - menuCompo.current.clientHeight;
-      console.log("scroll: ", d)
-      // console.log(menuCompo.current.classList)
-      if (d < 0) {
-        //menuCompo.current.classList.remove("bottom-[80px]")
-        //menuCompo.current.classList.add("bottom-[-220px]")
-        setIsUp(false)
-      } else {
-        //menuCompo.current.classList.remove("bottom-[-220px]")
-        //menuCompo.current.classList.add("bottom-[80px]")
-        setIsUp(true)
-      }
-    })
-  }, [])
+    document.addEventListener("scroll", evalscroll)
+    evalscroll()
+  })
+
+  // ${isUp?`bottom-[90px] xl:bottom-[90px]`:`bottom-[-235px] xl:bottom-[-235px]`} 
 
   return (
     <> 
-      {// <!-- https://github.com/tholman/github-corners -->
-      }
-
       <div 
          id={`social_corner_up`}
-        onMouseLeave={onLeaveHandler}
-        class="relative ml-3 px-2 m-3 mr-6 separateur scroll-ml-14 origin-[right_center] scale-1 transition-all duration-500">
+        class="relative ml-3 px-2 m-3 mr-6 separateur scroll-ml-14 origin-center scale-1 transition-all duration-500"
+      >
         <div>
           <button 
             onClick={onClickHandler} 
             type="button"
-            class="hover:cursor-pointer p-1 relative flex rounded-full text-sm focus:outline-none "
+            class={`hover:cursor-pointer p-1 relative flex rounded-full text-sm focus:outline-none shadow-[${`currentColor`}] ${isOpen?`shadow-lg bottom-1`:``}`}
             id={`user-menu-button-bottom-right${name}`}
             >
             <span class="absolute -inset-1.5"></span>
@@ -70,94 +63,81 @@ export default function SocialCornerUp( { urlOfLinkToShare = `https://www.twitch
               size={48}
               strokeWidth={4}
               stroke={`currentColor`}
-              //viewBox={"-6 -6 36 36"}
               viewBox={"-2 -4 32 32"}
               alt="Share"
               className="h-18 w-18 m-1 p-1 rounded-full"
             />
           </button>
         </div>
-
-        {
-          /**
-          *     <!--
-                  Dropdown menu, show/hide based on menu state.
-  
-                  Entering: "transition ease-out duration-100"
-                  From: "transform opacity-0 scale-95"
-                  To: "transform opacity-100 scale-100"
-                  Leaving: "transition ease-in duration-75"
-                  From: "transform opacity-100 scale-100"
-                  To: "transform opacity-0 scale-95"
-                -->
-          */
-        }
-        {/**
-         *   <div 
-         *     class="absolute top-[-150px] ml-4 right-0 z-10 mt-2 w-48 rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none"
-         *     role="menu"
-         *     aria-orientation="vertical"
-         *     aria-labelledby="user-menu-button"
-         *     tabIndex={-1}
-         *   ></div>
-         */}
-        {// isOpen &&
-        }
         <div
           ref={menuCompo} 
-          class={`${effet} ${isOpen?`scale-110`:`scale-0`} absolute ${isUp?`bottom-[80px] xl:bottom-[80x]`:`bottom-[-225px] xl:bottom-[-225px]`} z-10 mt-2 rounded-md bg-orange-500 py-1 shadow-lg ring-1 ring-orange ring-opacity-5 focus:outline-none`} role="menu" tabIndex={-1}>
-          {// <!-- Active: "bg-gray-100", Not Active: "" -->
-          }
-
-          <a href="https://www.twitch.tv/Justin_Curieux" role="menuitem" class="block" id={`user-menu-bottom-twitch${name}`}>
+          class={`
+            absolute 
+            origin-bottom
+            right-[38px]
+            flex
+            ${effet} 
+            ${isOpen?`scale-110`:`scale-0`} 
+            z-10 
+            mt-2 
+            bg-transparent 
+            py-1 
+            ${isUp?`rotate-[0deg] bottom-[90px]`:`rotate-[180deg] bottom-[-30px]`}
+            transform-gpu
+          `} 
+          role="menu" 
+          tabIndex={-1}
+        >
+          <a href="https://www.twitch.tv/Justin_Curieux" role="menuitem" 
+            class={`absolute rotate-[${isUp?`-70deg`:`180deg`}] bottom-[-60px] right-[20px]`}
+            id={`user-menu-bottom-twitch${name}`}>
             <TwitchIcon
               size={48}
               strokeWidth={2}
               stroke={`currentColor`}
-              //viewBox={"-6 -6 36 36"}
               viewBox={"-2 -4 32 32"}
               alt="Share"
               className="h-18 w-18 m-1 p-1 rounded-full"
             />
             <span class="sr-only">twitch</span>
           </a>
-          <a href="https://www.facebook.com/justin.curieux/" role="menuitem" class="block" id={`user-menu-bottom-youtube${name}`}>
+          <a href="https://www.facebook.com/justin.curieux/" role="menuitem" 
+            class={`absolute rotate-[${isUp?`-15deg`:`-180deg`}] bottom-[-30px] right-[-10px]`}
+            id={`user-menu-bottom-youtube${name}`}>
             <YoutubeIcon
               size={48}
               strokeWidth={2}
               stroke={`currentColor`}
-              //viewBox={"-6 -6 36 36"}
               viewBox={"-2 -4 32 32"}
               alt="Share"
               className="h-18 w-18 m-1 p-1 rounded-full"
             />
             <span class="sr-only">youtube</span>
           </a>
-          <a href="https://www.youtube.com/@justincurieux31" role="menuitem" class="block" id={`user-menu-bottom-facebook${name}`}>
+          <a href="https://www.youtube.com/@justincurieux31" role="menuitem" 
+            class={`absolute rotate-[${isUp?`15deg`:`-180deg`}] bottom-[-30px] right-[-50px]`}
+            id={`user-menu-bottom-facebook${name}`}>
             <FacebookIcon
               size={48}
               strokeWidth={2}
               stroke={`currentColor`}
-              //viewBox={"-6 -6 36 36"}
               viewBox={"-2 -4 32 32"}
               alt="Share"
               className="h-18 w-18 m-1 p-1 rounded-full"
             />
             <span class="sr-only">facebook</span>
           </a>
-          <a href="https://discord.com/channels/1054405752422420531/1054405753240305736" role="menuitem" class="block" id={`user-menu-bottom-discord${name}`}>
+          <a href="https://discord.com/channels/1054405752422420531/1054405753240305736" role="menuitem" 
+            class={`absolute rotate-[${isUp?`70deg`:`-180deg`}] bottom-[-60px] right-[-80px]`}
+            id={`user-menu-bottom-discord${name}`}>
             <DiscordIcon 
               fill={`currentColor`}
               classname="h-18 w-18 m-2 p-1 rounded-full"
             />
             <span class="sr-only">discord</span>
           </a>
-        </div>
-        
+        </div>        
       </div>
-
     </>
-
   );
 }
-
