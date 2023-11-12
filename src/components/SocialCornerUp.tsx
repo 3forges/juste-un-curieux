156864e8bb3baee93d9e49f5a49e4f7d2348c08c
bobo <bobo@pesto.io>
2023-11-12 08:47:25 +0100

    transform-gpu removed to fix social icons popup

--- src/components/SocialCornerUp.tsx
+++ src/components/SocialCornerUp.tsx
@@ -19,7 +19,6 @@ const effet: string = `
   origin-bottom-center
   ease-in-out 
   duration-300
-  transform-gpu
 `
 const iconSize: number = 48
 
@@ -48,11 +47,10 @@ export default function SocialCornerUp( { urlOfLinkToShare, name, menuShareItems
   return (
     <> 
       <div 
-         id={`social_corner_up`}
+        id={`social_corner_up`}
         class={`
           relative 
-          ml-6 px-2 m-3 mr-6 
-          separateur 
+          ml-6 px-2 m-3 mr-6  
           scroll-ml-14 
         `}
       >
@@ -60,10 +58,10 @@ export default function SocialCornerUp( { urlOfLinkToShare, name, menuShareItems
           <button 
             onClick={onClickHandler} 
             type="button"
-            class={`hover:cursor-pointer p-1 relative flex rounded-full text-sm focus:outline-none ${isOpen?`shadow-gray-400 shadow-lg bottom-1`:``}`}
+            class={`hover:cursor-pointer p-1 relative flex rounded-full text-sm ${isOpen?`shadow-gray-400 shadow-lg bottom-1`:``}`}
             id={`user-menu-button-bottom-right${name}`}
             >
-            <span class="absolute -inset-1.5"></span>
+
             <span class="sr-only">Open share menu</span>
             <Share2Icon
               size={48}
@@ -110,7 +108,7 @@ export default function SocialCornerUp( { urlOfLinkToShare, name, menuShareItems
                     stroke:`currentColor`,
                     viewBox:(item.label !== "Discord")?"-2 -4 32 32":"0 0 640 512",
                     alt:"Share",
-                    className:(item.label !== "Discord")?"m-1 p-1 rounded-full focus:border-none":"m-2 p-2 rounded-full focus:border-none"
+                    className:(item.label !== "Discord")?"m-1 p-1 rounded-full":"m-2 p-2 rounded-full"
                   })
                 }
                 <span class="sr-only">{item.label}</span>
