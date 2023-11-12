91de42ed2550c8c2aab3642829ee758d41c45a87
bobo <bobo@pesto.io>
2023-11-12 09:56:48 +0100

    socials as an Astro.content

--- src/components/SocialCornerUp.tsx
+++ src/components/SocialCornerUp.tsx
@@ -1,7 +1,7 @@
 import { useEffect } from 'preact/hooks'
 import { useState, useRef } from 'preact/compat'
 import { Share2Icon } from 'lucide-preact'
-import { socials, social } from "~/content/config"
+import { collections, social } from "~/content/config"
 
 export interface SocialCornerUpProps {
   urlOfLinkToShare: string
@@ -27,7 +27,7 @@ export default function SocialCornerUp( { urlOfLinkToShare, name, menuShareItems
   const [isUp, setIsUp] = useState<boolean>(true)
   const menuCompo = useRef<HTMLDivElement>(null)
 
-  console.log(socials, menuShareItems)
+  console.log(collections.socials, menuShareItems)
 
   const onClickHandler = () => {
     setIsOpen(!isOpen);
@@ -93,7 +93,7 @@ export default function SocialCornerUp( { urlOfLinkToShare, name, menuShareItems
           role="menu" 
           tabIndex={-1}
         >
-          {socials.map((item: social) => {
+          {collections.socials.map((item: social) => {
             return (
               <a 
                 href={`${item.link}?${urlOfLinkToShare}`}
--- src/content/config.tsx
+++ src/content/config.tsx
@@ -1,8 +1,6 @@
 import { defineCollection, z } from "astro:content";
-
-//import { h } from 'preact'
 import { TwitchIcon, YoutubeIcon, FacebookIcon } from 'lucide-preact'
-import type { LucideIcon, LucideProps } from 'lucide-preact'
+import type { LucideIcon } from 'lucide-preact'
 import DiscordIcon from "../components/DiscordIcon"
 
 export interface social {
@@ -13,44 +11,31 @@ export interface social {
   right: string
 }
 
-export const socials: social[] = [
+const socials: social[] = [
   {
     link: "https://www.twitch.tv/Justin_Curieux",
-    returnIcon: (props: LucideIcon) => {
-      //return h(TwitchIcon, {...props})
-      return (<TwitchIcon {...props} />)
-    }
-    ,
+    returnIcon: (props: LucideIcon) => { return <TwitchIcon {...props} /> },
     label: "Twitch",
     bottom: "-bottom-[20px]",
     right: "right-[20px]"
   },
   {
     link: "https://www.youtube.com/@justincurieux31",
-    returnIcon: (props: LucideIcon) => {
-      //return h(YoutubeIcon, {...props})
-      return (<YoutubeIcon {...props} />)
-    },
+    returnIcon: (props: LucideIcon) => { return <YoutubeIcon {...props} /> },
     label: "Youtube",
     bottom: "bottom-[10px]",
     right: "-right-[10px]"
   },
   {
     link: "https://www.facebook.com/justin.curieux/",
-    returnIcon: (props: LucideIcon) => {
-      //return h(FacebookIcon, {...props})
-      return (<FacebookIcon {...props} />)
-    },
+    returnIcon: (props: LucideIcon) => { return <FacebookIcon {...props} /> },
     label: "Facebook",
     bottom: "bottom-[10px]",
     right: "-right-[50px]"
   },
   {
     link: "https://discord.com/channels/1054405752422420531/1054405753240305736",
-    returnIcon: (props: LucideIcon) => {
-      //return h(DiscordIcon, {...props})
-      return (<DiscordIcon {...props} />)
-    },
+    returnIcon: (props: LucideIcon) => { return <DiscordIcon {...props} /> },
     label: "Discord",
     bottom: "-bottom-[15px]",
     right: "-right-[70px]"
@@ -69,4 +54,5 @@ const showcase = defineCollection({
 
 export const collections = {
   showcase,
+  socials,
 };
