5e353e1d009bab5aad2d283ba1e8cb89a0c23264
bobo <bobo@pesto.io>
2024-01-17 19:24:33 +0100

    youtube gallery pagination

--- src/components/VideoGallery.tsx
+++ src/components/VideoGallery.tsx
@@ -1,22 +1,36 @@
 import { urls } from "./videosUrls"
+import { useState } from "preact/hooks"
 
 export default function VideoGallery() {
+  const [pagination, setPagination] = useState<number>(0) 
+
+  const viewurls = urls.slice(pagination,pagination+1)
+  //const handlePage = (increment: number) => { alert(increment); setPagination(pagination+increment) }
+  console.log(pagination)
   return(
-    <>
+    <div class="border-black border-2">
+      <div class="text-white bg-black w-auto p-2">
+      ■ Gallerie des vidéos: 
+      <a onClick={async () => {setPagination(pagination-1)}}>◄</a> 
+      {pagination + 1} / {urls.length} 
+      <a onClick={async () => {setPagination(pagination+1)}}>►</a>
+      </div>
+      <div class="flex flex-row flex-auto p-1 gap-x-4">
       {
-        urls.map( (url: string) => {
+        viewurls.map( (url: string) => {
           // return <a href={`https://www.youtube.com/watch?v=${url}`}>{url}</a>
           return (
             <iframe width="560" height="315" 
               src={`https://www.youtube.com/embed/${url}?si=BUW-Hf9r-yCHLET`} 
               title="YouTube video player" 
-              frameborder="0" 
+              frameBorder="0" 
               allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
-              allowfullscreen>
+              allowFullScreen>
             </iframe>
           )
         }
       )}
-    </>
+      </div>
+    </div>
   )
 }
