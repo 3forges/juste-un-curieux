fd954c5b2ac66cbcb2b788ec44cd5204c3994c57
bobo <bobo@pesto.io>
2024-01-19 10:42:00 +0100

    video gallery pagination hard code

--- src/components/VideoGallery.tsx
+++ src/components/VideoGallery.tsx
@@ -3,24 +3,24 @@ import { useState } from "preact/hooks"
 
 export default function VideoGallery() {
   const [pagination, setPagination] = useState<number>(0) 
-
-  const viewurls = urls.slice(pagination,pagination+1)
+  const paginationItemsNumber = 4
+  const viewurls = urls.slice(pagination,pagination+paginationItemsNumber)
   //const handlePage = (increment: number) => { alert(increment); setPagination(pagination+increment) }
   console.log(pagination)
   return(
-    <div class="border-black border-2">
-      <div class="text-white bg-black w-auto p-2">
+    <div class="m-2">
+      <div class="text-white bg-black w-auto p-2 rounded-t-lg">
       ■ Gallerie des vidéos: 
-      <a onClick={async () => {setPagination(pagination-1)}}>◄</a> 
+      <a onClick={async () => {setPagination(pagination-paginationItemsNumber)}}>◄</a> 
       {pagination + 1} / {urls.length} 
-      <a onClick={async () => {setPagination(pagination+1)}}>►</a>
+      <a onClick={async () => {setPagination(pagination+paginationItemsNumber)}}>►</a>
       </div>
-      <div class="flex flex-row flex-auto p-1 gap-x-4">
+      <div class="flex flex-row flex-auto p-1 gap-x-4 border-black border-2">
       {
         viewurls.map( (url: string) => {
           // return <a href={`https://www.youtube.com/watch?v=${url}`}>{url}</a>
           return (
-            <iframe width="560" height="315" 
+            <iframe 
               src={`https://www.youtube.com/embed/${url}?si=BUW-Hf9r-yCHLET`} 
               title="YouTube video player" 
               frameBorder="0" 
