31af3196e7ed479f8ac490ade422da806d73b30d
Jean-Baptiste Lasselle <jean.baptiste.lasselle@gmail.com>
2024-01-22 19:44:21 +0100

    video gallery: pagination arrows better constrast

--- src/components/pagination/PaginationLeftButton.tsx
+++ src/components/pagination/PaginationLeftButton.tsx
@@ -20,8 +20,9 @@ export default function PaginationButtonLeft(props: PaginationLeftButtonProps) {
     let isEnabled = props.paginationState > 0 // + 1 < Math.ceil(props.itemsNumber / props.itemsPerPage) - 1
     SetDisabled(!isEnabled)
   }
-  const ChevronDisabledCss = `text-gray-400 pl-0`
-  const ChevronEnabledCss = `hover:cursor-pointer pl-0`
+  // const ChevronDisabledCss = `text-gray-400 pl-0`
+  const ChevronDisabledCss = `text-gray-600 pl-0`
+  const ChevronEnabledCss = `text-gray-200 hover:cursor-pointer pl-0`
   // const btnStyling = `text-white bg-white-700 hover:bg-white-800 focus:outline-none focus:ring-4 focus:ring-white-300 font-medium rounded-full text-sm p-0 text-center me-2 mb-2 dark:bg-white-600 dark:hover:bg-white-700 dark:focus:ring-white-800`
   const btnStyling = `ring-1 ring-white-900 text-white bg-transparent hover:bg-white-300 focus:outline-none focus:ring-4 focus:ring-white-800 font-medium rounded-full text-sm p-0 text-center me-0 mb-0 focus:p-[1px] p-[.5px] dark:bg-white-600 dark:hover:bg-white-700 dark:focus:ring-white-800`
   if (disabled) {
