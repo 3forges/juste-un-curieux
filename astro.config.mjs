6b357e7295e85cc6770ab4a005670cfc5ccadded
Jean-Baptiste Lasselle <jean.baptiste.lasselle@gmail.com>
2024-01-23 11:09:58 +0100

    test seo image

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,7 +8,7 @@ import preact from "@astrojs/preact";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://astro-moon-landing.netlify.app/",
+  site: "https://justincurieux-boris-dropup-refined-pkfwversm.vercel.app/",
   integrations: [tailwind(), preact()],
   vite: {
     ssr: {
