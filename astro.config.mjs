c22dd06ab681d49293b29e66ad11e22dc61f9dc6
bobo <bobo@pesto.io>
2024-01-27 21:12:08 +0100

    docs repushed

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,10 +3,10 @@ import tailwind from "@astrojs/tailwind";
 import icon from "astro-icon";
 import preact from "@astrojs/preact";
 
-
+export const baseURL = `/juste-un-curieux`
 // https://astro.build/config
 export default defineConfig({
   site: "https://3forges.github.io",
-  base: "/juste-un-curieux",
+  base: `${baseURL}`,
   integrations: [tailwind(), icon(), preact({ compat: true })]
 });
