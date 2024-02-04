7555e687380ac8db7466fae0f107ee73d0aa43ee
Jean-Baptiste Lasselle <jean.baptiste.lasselle@gmail.com>
2024-02-04 18:04:06 +0100

    the preact component for youtube livestream does pickup a started planified live which started

--- src/config.ts
+++ src/config.ts
@@ -1,3 +1,14 @@
 export const robotsConfig = {
   disableIndexing: false
 }
+export const youtube = {
+  channel_id: `UCRCe4BKXU3gNhwAGwheHYyw`, // Justin Curieux's Channel ID is [UCRCe4BKXU3gNhwAGwheHYyw]
+  user_id: `idontknowitsnotusedyetanyway` // but just know that user id and channel id are different
+}
+
+export const tests = {
+  youtube : {
+  channel_id: `UCGymsagsg1lUxDb2qxjK6Lg`, // Justin Curieux's Channel ID is [UCRCe4BKXU3gNhwAGwheHYyw]
+  user_id: `Gymsagsg1lUxDb2qxjK6Lg`
+  }
+}
