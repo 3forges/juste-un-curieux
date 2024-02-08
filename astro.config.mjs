a35f6b89f69f9e1d273b0bd3edd4cc4a5fa19eb0
Jean-Baptiste Lasselle <jean.baptiste.lasselle@gmail.com>
2024-02-08 21:36:59 +0100

    i solved the issue about fsevents

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,12 +4,13 @@ import preact from "@astrojs/preact";
 import robotsTxt from "astro-robots-txt";
 // import { astroImageTools } from "astro-imagetools";
 
-console.log(` [astro.config.mjs] --->>> DEPLOYMENT_ASTRO_BASE_CONFIG = [${process.env.DEPLOYMENT_ASTRO_BASE_CONFIG}]`);
-const ASTRO_BASE_CONFIG_ARRAY = (process.env.DEPLOYMENT_ASTRO_BASE_CONFIG?process.env.DEPLOYMENT_ASTRO_BASE_CONFIG.split(`/`):[""]);
-console.log(` [astro.config.mjs] --->>> ASTRO_BASE_CONFIG_ARRAY = `, ASTRO_BASE_CONFIG_ARRAY);
+import { myDEPLOYMENT_ASTRO_SITE_CONFIG, myDEPLOYMENT_ASTRO_BASE_CONFIG } from './src/config'
+// console.log(` [astro.config.mjs] --->>> DEPLOYMENT_ASTRO_BASE_CONFIG = [${process.env.DEPLOYMENT_ASTRO_BASE_CONFIG}]`);
+// const ASTRO_BASE_CONFIG_ARRAY = (process.env.DEPLOYMENT_ASTRO_BASE_CONFIG?process.env.DEPLOYMENT_ASTRO_BASE_CONFIG.split(`/`):[""]);
+// console.log(` [astro.config.mjs] --->>> ASTRO_BASE_CONFIG_ARRAY = `, ASTRO_BASE_CONFIG_ARRAY);
 
-export const myDEPLOYMENT_ASTRO_SITE_CONFIG = process.env.DEPLOYMENT_ASTRO_SITE_CONFIG;
-export const myDEPLOYMENT_ASTRO_BASE_CONFIG = `/${ASTRO_BASE_CONFIG_ARRAY[ASTRO_BASE_CONFIG_ARRAY.length - 1]}`;
+// export const myDEPLOYMENT_ASTRO_SITE_CONFIG = process.env.DEPLOYMENT_ASTRO_SITE_CONFIG;
+// export const myDEPLOYMENT_ASTRO_BASE_CONFIG = `/${ASTRO_BASE_CONFIG_ARRAY[ASTRO_BASE_CONFIG_ARRAY.length - 1]}`;
 console.log(` [astro.config.mjs] --->>> DEPLOYMENT_ASTRO_SITE_CONFIG = [${myDEPLOYMENT_ASTRO_SITE_CONFIG}]`);
 console.log(` [astro.config.mjs] --->>> DEPLOYMENT_ASTRO_BASE_CONFIG = [${myDEPLOYMENT_ASTRO_BASE_CONFIG}]`);
 // https://astro.build/config
